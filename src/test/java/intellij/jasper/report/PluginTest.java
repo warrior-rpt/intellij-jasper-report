30a05648bd89466b258fd2879f60b36de88a6c46
warrior-rpt <Rithin@MacBook-Pro.local>
2026-01-07 13:54:52 -0800

    upraded the plugins and ideaVersion to the latest

---
+++ src/test/java/intellij/jasper/report/PluginTest.java
@@ -0,0 +1,11 @@
+package test.java.intellij.jasper.report;
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+
+public class PluginTest extends BasePlatformTestCase {
+
+    public void testProjectService() {
+        // Just verify the test environment is working and the plugin is loaded
+        assertNotNull("Project instance should not be null", getProject());
+    }
+}
